e4206e120c7f5efa1df912772838cd9df7287c8a
Sophia S <82449876+SophiaSaks@users.noreply.github.com>
2023-01-30 22:12:55 +0100

    initialized project

---
+++ color-palette-generator/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className='Navbar'>
+      <h1 className='Navbar__title'>Create a Color Palette</h1>
+
+    </div>
+  );
+}
+
+export default Navbar;
