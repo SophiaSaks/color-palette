ed05bbcccd9accf31aa64110e3941c4d7700861e
Sophia S <82449876+SophiaSaks@users.noreply.github.com>
2023-02-02 18:22:54 +0100

    added color pickers and css

--- color-palette-generator/src/components/ColorGenerator.tsx
+++ color-palette-generator/src/components/ColorGenerator.tsx
@@ -3,19 +3,73 @@ import { SketchPicker } from 'react-color';
 import { useState } from 'react'; 
 
 function ColorGenerator() {
-  const [currentBackground, setCurrentBackground] = useState('#FFF');
-  // const handleOnChange = (color) => {
-  //   setCurrentBackground(color)
-  // }
+  const [currentBackground1, setCurrentBackground1] = useState('#FFF');
+  const [currentBackground2, setCurrentBackground2] = useState('#FFF');
+  const [currentBackground3, setCurrentBackground3] = useState('#FFF');
+
+  const handleOnChange1 = (color: {hex: string}) => {
+    setCurrentBackground1(color.hex);
+  }
+  const handleOnChange2 = (color: {hex: string}) => {
+    setCurrentBackground2(color.hex);
+  }
+
+  const handleOnChange3 = (color: {hex: string}) => {
+    setCurrentBackground3(color.hex);
+  }
+  
+  const appStyle1 = {
+    color: '#494949',
+    'background-color': currentBackground1,
+    border: '0.5rem solid #FFD699',
+    width: '100%',
+  }
+
+  const appStyle2 = {
+    color: '#494949',
+    'background-color': currentBackground2,
+    border: '0.5rem solid #FFD699',
+    width: '100%',
+  }
+
+  const appStyle3 = {
+    color: '#494949',
+    'background-color': currentBackground3,
+    border: '0.5rem solid #FFD699',
+    width: '100%',
+  }
 
   return (
-    <div className='ColorGenerator'>
-        <SketchPicker 
-        color={currentBackground}
-        // onChangeComplete={handleOnChange(color)}
-        />
-       
+    <div className='ColorGenerator__mainDiv'>
+      <div className='ColorGenerator' style={appStyle1}>
+          <SketchPicker 
+          color={currentBackground1}
+          onChangeComplete={handleOnChange1}
+          />
+          <h1 className='ColorGenerator__colorText'>Hex-code: {currentBackground1}</h1>
+        
 
+      </div>
+
+      <div className='ColorGenerator' style={appStyle2}>
+          <SketchPicker 
+          color={currentBackground2}
+          onChangeComplete={handleOnChange2}
+          />
+          <h1 className='ColorGenerator__colorText'>Hex-code: {currentBackground2}</h1>
+    
+
+      </div>
+
+      <div className='ColorGenerator' style={appStyle3}>
+          <SketchPicker 
+          color={currentBackground3}
+          onChangeComplete={handleOnChange3}
+          />
+          <h1 className='ColorGenerator__colorText'>Hex-code: {currentBackground3}</h1>
+    
+
+      </div>
     </div>
   );
 }
